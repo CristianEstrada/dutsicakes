434adc47410d949968339cdf5b57d6e2c060ed56
Cristian Rodrigo Estrada Romero <47439120+CristianEstrada@users.noreply.github.com>
2025-10-07 13:19:00 -0700

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,6 @@ window.addEventListener("load", () => {
     const modal = document.getElementById("welcome-modal");
     setTimeout(() => {
       modal.classList.add("hidden");
-    }, 1500); // 2 segundos, ajusta según lo que quieras
+    }, 1500); 
   });
   
